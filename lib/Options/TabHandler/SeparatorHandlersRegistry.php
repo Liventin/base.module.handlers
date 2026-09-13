eccad22a7dbd790fbd08c5e3d42252327c97774a
Liventin <103806534+Liventin@users.noreply.github.com>
2026-09-13 22:37:00 +0300

    Update and rename Separator.php to SeparatorHandlersRegistry.php

--- lib/Options/TabHandler/SeparatorHandlersRegistry.php
+++ lib/Options/TabHandler/SeparatorHandlersRegistry.php
@@ -6,7 +6,7 @@ use Base\Module\Options\TabHandler;
 use Base\Module\Service\Options\Option;
 use Bitrix\Main\Localization\Loc;
 
-class Separator implements Option
+class SeparatorHandlersRegistry implements Option
 {
     public static function getId(): string
     {
